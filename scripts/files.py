9e58bff2ede5dbe731575719c8e2013120499273
crumpstrr33 <jscott128@tuta.io>
2026-05-21 10:44:22 -0700

    Changed input params for methods - `verify_data` and `load_data` now take in Path objects instead of params - This is compensated by new `find_dir` method that return this Path object when given specified params - `parse_data` no longer used regex, strings are parse more explicitly - `load_falqon_depth` added; it replaces `create_falqon_depths` formally in `postdata.py` - Update type hints

--- scripts/files.py
+++ scripts/files.py
@@ -1,81 +1,73 @@
 import re
 from collections import defaultdict
-from typing import Optional, Union
+from collections.abc import Sequence
+from pathlib import Path
+from typing import TypeAlias
 
 import numpy as np
 from numpy.typing import NDArray
 
-from .constants import INVMS, OUTPUT_DIR
-from .data import split_data
-from .events import get_data
+from .constants import (
+    DATA_CHOICES,
+    DEFAULT_BETA0,
+    DEFAULT_DICT,
+    DEFAULT_DT,
+    DEFAULT_DTAU,
+    EVENT_CHOICES,
+    HAMILTONIAN_CHOICES,
+    INVMS,
+    LAMBDA_COMBOS,
+    NORM_CHOICES,
+    NUM_FSP_DICT,
+    OUTPUT_DIR,
+    SYM_TRUE_BS_DICT,
+)
+from .hamiltonians import get_bitstrings
+from .type_hints import DatumType
+
+ParamType: TypeAlias = str | float | int
 
 
-def parse_data() -> NDArray[NDArray[str]]:
+def _parse_extras(extras: str, kw: str, default: ParamType) -> str:
     """
-    Searches the data directory defined by `OUTPUT_DIR` and finds the params of
-    every directory.
+    Parses the string `extras` for the keyword `kw` and returns the number that
+    comes immediately after it. If not found, returns `default`.
+
+    We are assuming that the default value is number-like.
     """
-    # Regex strign for extracting info from directory name
-    ch = "[a-zA-Z0-9]"
-    # re_dir = rf"^({ch}+)_({ch}+)_(\d+)_({ch}+)(?:-({ch}+)-({ch}+))?_({ch}+)_(\d+)$"
-    re_dir = rf"^({ch}+)_({ch}+)_(\d+)_({ch}+)(?:-({ch}+)-({ch}+))?_({ch}+)$"
-
-    alg_dirs = OUTPUT_DIR.iterdir()
-    params = []
-    for alg_dir in alg_dirs:
-        alg = alg_dir.name
-
-        param_dirs = alg_dir.iterdir()
-        for param_dir in param_dirs:
-            (
-                etype,
-                dtype,
-                depth,
-                hamiltonian,
-                lambda_nume,
-                lambda_denom,
-                norm,
-            ) = re.findall(re_dir, param_dir.name)[0]
-
-            if hamiltonian == "H2" and (lambda_nume == "" or lambda_denom == ""):
-                raise Exception("Hamiltonian is H2, both lambda kwargs must be defined")
-
-            params.append(
-                [
-                    alg,
-                    depth,
-                    hamiltonian,
-                    norm,
-                    etype,
-                    dtype,
-                    lambda_nume,
-                    lambda_denom,
-                ]
-            )
-
-    return np.array(params, dtype=str)
+    # Round to 3 decimal points to be consistent
+    return next(
+        (f"{float(s.removeprefix(kw)):.3f}" for s in extras if s.startswith(kw)),
+        f"{float(default):.3f}",
+    )
 
 
-def verify_data(
+def _check_for_error(name: str, val: ParamType, options: Sequence[ParamType]):
+    """
+    Error for when a parameter, `name` is not one of the required values found
+    in `options` but is instead `val`.
+    """
+    if val not in options:
+        raise ValueError(f"{name} is {val}. It must be one of the following: {options}")
+
+
+def find_dir(
     alg: str,
-    depth: Union[int, str],
+    depth: int | str,
     hamiltonian: str,
     norm: str,
     etype: str = "ttbar",
     dtype: str = "parton",
-    lambda_nume: Optional[str] = None,
-    lambda_denom: Optional[str] = None,
-) -> bool:
+    lambda_nume: str | None = None,
+    lambda_denom: str | None = None,
+    **extras: str,
+) -> Path:
     """
-    Makes basic checks on the saved data. If this function returns True, then
-    the data is available to be analyzed. It checks
-        2) That none of the events are duplicated
-        3) That there are no events missing
-    It doesn't check if there are 12,000 / 6 = 2,000 total events found.
+    For the given parameters, returns the directory path as a Path object.
 
     Parameters:
     alg - The algorithm used for the data. Currently can be "qaoa", "maqaoa",
-        "xqaoa", or "falqon".
+        "xqaoa", "varqite", or "falqon".
     depth - The depth of the circuit ran
     hamiltonian - Which Hamiltonian used. Can be "H0", "H1", or "H2". If "H2",
         must define `lambda_nume` and `lambda_denom`.
@@ -90,18 +82,85 @@ def verify_data(
     lambda_denom (default None) - The denominator of the lambda coefficient used
         in the H2 Hamiltonian.
     """
-    # Find the total number of events per invariant mass bin
-    depth = int(depth)
-
     # Find the root directory for data
     ham_str = hamiltonian
     if hamiltonian == "H2":
         ham_str += f"-{lambda_nume}-{lambda_denom}"
-    root_dir = OUTPUT_DIR / alg / f"{etype}_{dtype}_{depth}_{ham_str}_{norm}"
+    dir_name = f"{etype}_{dtype}_{depth}_{ham_str}_{norm}"
+    # We add on the algorithm-specific parameters ONLY IF they aren't the defaul
+    dir_name += "_".join(
+        [""] + ["".join((k, v)) for k, v in extras.items() if DEFAULT_DICT[k] != float(v)]
+    )
+    return OUTPUT_DIR / alg / dir_name
 
+
+def parse_data() -> NDArray[NDArray[str]]:
+    """
+    Searches the data directory defined by `OUTPUT_DIR` and finds the params of
+    every directory.
+    """
+    alg_dirs = OUTPUT_DIR.iterdir()
+    params = []
+    # Loop through the directories of each algorithm
+    for alg_dir in alg_dirs:
+        alg = alg_dir.name
+
+        param_dirs = alg_dir.iterdir()
+        # Loop through each directory representing a choice of parameters
+        for param_dir in param_dirs:
+            split_name = param_dir.name.split("_")
+
+            etype = split_name[0]
+            dtype = split_name[1]
+            depth = split_name[2]
+            ham = split_name[3].split("-")
+            norm = split_name[4]
+            extras = split_name[5:]
+
+            # Make sure all these values are valid
+            _check_for_error("Event type", etype, EVENT_CHOICES)
+            _check_for_error("Data type", dtype, DATA_CHOICES)
+            _check_for_error("Hamiltonian", ham[0], HAMILTONIAN_CHOICES)
+            if len(ham) > 1:
+                _check_for_error("Lambda denominator", ham[1], LAMBDA_COMBOS)
+                _check_for_error("Lambda numerator", ham[2], LAMBDA_COMBOS)
+            _check_for_error("Normalization", norm, NORM_CHOICES)
+
+            # Get algorithm-specific info if it's there
+            if alg == "falqon":
+                beta0 = _parse_extras(extras, "beta0", DEFAULT_BETA0)
+                dt = _parse_extras(extras, "dt", DEFAULT_DT)
+                extras = [beta0, dt]
+            elif alg == "varqite":
+                dtau = _parse_extras(extras, "dtau", DEFAULT_DTAU)
+                extras = [dtau, ""]
+            else:
+                extras = ["", ""]
+
+            lambda_type = ["", ""] if len(ham) == 1 else ham[1:]
+            datum = [alg, depth, ham[0], norm, etype, dtype, *lambda_type, *extras]
+            params.append(datum)
+
+    return np.array(params, dtype=str)
+
+
+def verify_data(
+    dir_path: Path,
+) -> bool:
+    """
+    Makes basic checks on the saved data. If this function returns True, then
+    the data is available to be analyzed. It checks
+        2) That none of the events are duplicated
+        3) That there are no events missing
+    It doesn't check if there are 12,000 / 6 = 2,000 total events found.
+
+    Parameters:
+    dir_path: Path to the directory with the data. That is, it will have
+        directories labeled by the invariant mass bin lower values.
+    """
     # Loop over the invariant mass subdirectories
     for invm in INVMS[:-1]:
-        invm_dir = root_dir / f"{invm:.2f}"
+        invm_dir = dir_path / f"{invm:.2f}"
         if not invm_dir.exists():
             print(f"{invm:.2f} -- Invariant mass file {invm_dir} does not exist.")
             return False
@@ -127,38 +186,29 @@ def verify_data(
         except ValueError:
             print(f"{invm_dir} is empty.")
             return False
+
         # Make sure we cover every event and every event only once
         all_inds = np.arange(0, max_ind)
         for pair in ind_pairs:
             inds = np.arange(*pair)
             # Make sure we don't have duplicates of events
-            if not np.all(np.in1d(inds, all_inds)):
-                p_str = f"{etype} | {dtype} | {alg} | p={depth} | {norm} | {hamiltonian} "
-                if lambda_nume is not None:
-                    p_str += f" {lambda_nume} / {lambda_denom}"
-                print(
-                    f"{p_str}\n{invm:.2f} -- Found duplicate indices looking at range {pair}"
-                )
+            if not np.all(np.isin(inds, all_inds)):
+                print(f"Path: {invm_dir}\nFound duplicates at {pair}.")
                 return False
 
             all_inds = all_inds[~np.isin(all_inds, inds)]
 
         if len(all_inds):
-            print(f"{invm:.2f} -- Missing {len(all_inds)} events: {all_inds}")
+            print(f"Path: {invm_dir}\nMissing {len(all_inds)} events: {all_inds}.")
             return False
+
+    # All checks passed
     return True
 
 
 def load_data(
-    alg: str,
-    depth: Union[int, str],
-    hamiltonian: str,
-    norm: str,
-    etype: str = "ttbar",
-    dtype: str = "parton",
-    lambda_nume: Optional[str] = None,
-    lambda_denom: Optional[str] = None,
-) -> dict[float, dict[str, NDArray[np.float64]]]:
+    dir_path: Path,
+) -> DatumType:
     """
     Return a dictionary with the data for a given run of events. The returned
     data structure is a dictionary with keys representing the invariant mass
@@ -169,32 +219,13 @@ def load_data(
     them together.
 
     Parameters:
-    alg - The algorithm used for the data. Currently can be "qaoa", "maqaoa",
-        "xqaoa", or "falqon".
-    depth - The depth of the circuit ran
-    hamiltonian - Which Hamiltonian used. Can be "H0", "H1", or "H2". If "H2",
-        must define `lambda_nume` and `lambda_denom`.
-    norm - The normalization scheme used for the coefficient matrix. Can be
-        "max", "mean", or "sum".
-    etype (default "ttbar") - The event type, currently can be "ttbar",
-        "tW" or "6jet".
-    dtype (default "parton") - The data type, currently can be "parton"
-        or "smeared".
-    lambda_nume (default None) - The numerator of the lambda coefficient used in
-        the H2 Hamiltonian.
-    lambda_denom (default None) - The denominator of the lambda coefficient used
-        in the H2 Hamiltonian.
+    dir_path: Path to the directory with the data. That is, it will have
+        directories labeled by the invariant mass bin lower values.
     """
-    # Find the root directory for data
-    ham_str = hamiltonian
-    if hamiltonian == "H2":
-        ham_str += f"-{lambda_nume}-{lambda_denom}"
-    root_dir = OUTPUT_DIR / alg / f"{etype}_{dtype}_{depth}_{ham_str}_{norm}"
-
     data = {}
     # Iterate over the invariant masses
     for invm in INVMS[:-1]:
-        invm_dir = root_dir / f"{invm:.2f}"
+        invm_dir = dir_path / f"{invm:.2f}"
 
         invm_data = defaultdict(list)
         # Iterate over the different files
@@ -221,3 +252,85 @@ def load_data(
 
         data[invm] = dict(invm_data)
     return data
+
+
+def _extract_falqon_data(
+    dfile: Path, depths: NDArray[int], correct: str, bitstrings: NDArray[str]
+) -> list[DatumType]:
+    """
+    Properly extract the FALQON data for specific depths.
+    """
+    file_data = np.load(dfile)
+    data = []
+
+    invm_p4s = file_data["invm_p4s"]
+    invms = file_data["invms"]
+    coeffs = file_data["coeffs"]
+    norm_coeffs = file_data["norm_coeffs"]
+    min_bitstrings = file_data["min_bitstrings"]
+    min_energies = file_data["min_energies"]
+    costs = file_data["costs"]
+    betas = file_data["betas"]
+
+    # Get probabilities as specific depth
+    depth_probs = np.swapaxes(file_data["depth_probs"][:, depths - 1, :], 0, 1)
+    # Get expectation values at specific depth
+    depth_expvals = np.swapaxes(file_data["costs"][:, depths - 1], 0, 1)
+    for depth, probs, expvals in zip(depths, depth_probs, depth_expvals):
+        datum = {}
+
+        # Per event data, isn't dependent on depth
+        datum["invm_p4s"] = invm_p4s
+        datum["invms"] = invms
+        datum["coeffs"] = coeffs
+        datum["norm_coeffs"] = norm_coeffs
+        datum["min_bitstrings"] = min_bitstrings
+        datum["min_energies"] = min_energies
+
+        # Depth-dependent data
+        datum["costs"] = costs[:, :depth]
+        datum["expvals"] = expvals
+        datum["probs"] = probs
+        datum["betas"] = betas[:, :depth]
+
+        data.append(datum)
+    return data
+
+
+def load_falqon_depths(falqon_path: Path, depths: Sequence[int]) -> list[DatumType]:
+    """
+    FALQON data is stored for EVERY depth (since each depth is ran iteratively).
+    Thus, this method will extract the data for only the depths specified.
+
+    Parameters:
+    falqon_path: Path to the directory with the data. That is, it will have
+        directories labeled by the invariant mass bin lower values.
+    depths: Depths to extract data for.
+    """
+    depths = np.array(depths)
+    # Get constants to use
+    etype = falqon_path.name.split("_")[0]
+    num_fsp = NUM_FSP_DICT[etype]
+    correct = SYM_TRUE_BS_DICT[etype]
+    bitstrings = get_bitstrings(N=num_fsp, astype="bits")
+    num_p = len(depths)
+
+    falqon_data = [{} for _ in range(num_p)]
+    for invm in INVMS[:-1]:
+        path = falqon_path / f"{invm:.2f}"
+
+        invm_data = [defaultdict(list) for _ in range(num_p)]
+        for dfile in path.iterdir():
+            # This is where the FALQON-specific parsing occurs
+            partial_data = _extract_falqon_data(dfile, depths, correct, bitstrings)
+
+            for invm_datum, partial_datum in zip(invm_data, partial_data):
+                for key, val in partial_datum.items():
+                    invm_datum[key].append(val)
+
+        # Add together and concatenate individual files
+        for falqon_datum, invm_datum in zip(falqon_data, invm_data):
+            falqon_datum[invm] = {k: np.concatenate(v) for k, v in invm_datum.items()}
+        del invm_data
+
+    return falqon_data
