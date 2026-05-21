10e418dec60952a73bf40e6e09f225ada2921449
crumpstrr33 <jscott128@tuta.io>
2026-05-21 10:28:31 -0700

    Lots of updates - Updated `find_efficiency` to NOT use `ranks` or `rank_probs` since those are wrong. Calculates from the probabilities. - Removed `create_falqon_depth`. Replaced with `load_falqon_depth` in `files.py`. The former took in a full data file from FALQON whereas the latter now just takes in the data path to s ave memory usage. - Added `find_probabilities` which is equivalent to `find_efficiencies` but finds the mean probabilities of the bitstring(s) in question, e.g. it is a measure of confidence of the algorithm. - `parse_with_metadata` can now take in a single query (as a list of strings) instead of requiring a list of queries. - Changed `get_bitstrings` to `get_chosen_bitstrings` to avoid clashing with `get_bitstrings` from `hamiltonians.py` - Updated type hints

--- scripts/postdata.py
+++ scripts/postdata.py
@@ -1,78 +1,27 @@
-from typing import Sequence, Union
+from collections.abc import Sequence
+from typing import Literal
 
 import numpy as np
 from numpy.typing import NDArray
 
-from .constants import INVMS, NUM_FSP_DICT, SYM_TRUE_BS_DICT
+from .constants import INVMS, SYM_TRUE_BS_DICT
 from .data import get_bitstring_invms
-from .type_hints import datum_type
-
-
-def create_falqon_depth(
-    datum: datum_type, metadatum: NDArray[str], depth: int
-) -> tuple[datum_type, NDArray[str]]:
-    """
-    Since the FALQON data comes with the probabilities for every depth, as each
-    circuit depth is created, this method returns in the proper format the
-    `metadatum` and `datum` for a specific depth.
-
-    Parameters:
-    datum - The data for this run as created by the `JobRunner` class and saved
-        as an .npz file.
-    metadatum - An element of the list output by the `parse_data` method.
-        Contains the metadata info for a run.
-    depth - The depth to get the info for.
-    """
-    num_fsp = NUM_FSP_DICT[metadatum[4]]
-    correct = SYM_TRUE_BS_DICT[metadatum[4]]
-    bitstrings = np.array([format(x, f"0{num_fsp}b") for x in range(2**num_fsp)])
-
-    if metadatum[0] != "falqon":
-        raise Exception(f"Not a FALQON instance:\n{metadatum}")
-    if "depth_probs" not in datum[1.00]:
-        raise Exception("No `depth_probs` key. Might be a copy already...")
-    is_full_depth = depth == datum[2.50]["depth_probs"].shape[1]
-
-    new_datum = {}
-    for invm in INVMS[:-1]:
-        if is_full_depth:
-            invm_datum = {k: v for k, v in datum[invm].items() if k != "depth_probs"}
-        else:
-            invm_datum = {
-                k: v
-                for k, v in datum[invm].items()
-                if k
-                not in ["probs", "depth_probs", "ranks", "rank_probs", "costs", "betas"]
-            }
-            probs = datum[invm]["depth_probs"][:, depth, :]
-            invm_datum["probs"] = probs
-            invm_datum["rank_probs"] = probs[:, int(correct, 2)]
-            invm_datum["costs"] = datum[invm]["costs"][:, :depth]
-            invm_datum["betas"] = datum[invm]["betas"][:, :depth]
-
-            # Get indices of sorted probabilities for each event
-            sorted_inds = np.flip(np.argsort(probs, axis=1), axis=1)
-            ranks = np.where(bitstrings[sorted_inds] == correct)[1]
-            # This is the assumption that the bitstring is symmetric, so rank = 1
-            # is the same as rank == 0, so subtract 1 from all odd ranks
-            ranks -= ranks % 2
-            invm_datum["ranks"] = ranks
-
-        new_datum[invm] = invm_datum
-
-    new_metadatum = np.concatenate(([metadatum[0], depth], metadatum[2:]))
-    return new_datum, new_metadatum
+from .hamiltonians import swap
+from .type_hints import DatumType
 
 
 def find_efficiency(
-    data: NDArray[datum_type],
+    data: NDArray[DatumType],
     find_correct: bool = True,
     assume_symmetric: bool = True,
+    etype: str | Sequence[str] = "ttbar",
 ) -> NDArray[NDArray[float]]:
     """
     From the given data arrays, finds the efficiency. That is, it finds the
     fraction of events for which the algorithm found the "correct" bitstring
-    with the largest probability.
+    with the largest probability OR the "minimum" bitstring. Keep in mind, we
+    are assuming that symmetric bitstring are equivalent, i.e. we are summing
+    both cases together.
 
     Parameters:
     data - An array of data for runs as created by the `JobRunner` class and
@@ -80,43 +29,117 @@ def find_efficiency(
     find_correct (default True) - If True, the "correct" bitstring is the one
         that solves the problem, in our case it is "000111". If False, the
         "correct" bitstring is the one that minimizing the Hamiltonian.
-    assume_symmetric (default True) - If True, it will assume that symmetric
-        bitstrings are equivalent. So a rank of 1 would be equivalent to a rank
-        of 0 and so on.
+    etype (default "ttbar") - The event type. If a string, it will apply it the
+        same data type to every element of `data`. If a list, it will be
+        element-specific.
     """
-    effs = []
-    # Iterate for each run
-    for datum in data:
-        datum_effs = []
-        # Iterate for each invariant mass bin
-        for invm in INVMS[:-1]:
-            # Calculate the efficiency with possibility of symmetry
+    # Number of data to go over
+    data_len = len(data)
+    n_bins = len(INVMS) - 1
+    # Turn into a list of all the same etype
+    if isinstance(etype, str):
+        etype = [etype] * data_len
+
+    effs = np.empty((data_len, n_bins), dtype=np.float64)
+    for dind, (datum, etype) in enumerate(zip(data, etype)):
+        # The correct bitstring
+        cor_bs = SYM_TRUE_BS_DICT[etype]
+
+        for iind, invm in enumerate(INVMS[:-1]):
+            invm_datum = datum[invm]
+            # Bitstrings minimizing the energy for each event
+            min_bitstrings = invm_datum["min_bitstrings"]
+            # All probabilities for each event
+            evt_probs = invm_datum["probs"]
+
+            # For each event, index of max probability, i.e. base 10 of bitstring
+            max_probs_inds = np.argmax(evt_probs, axis=1)
+            tot_evts = max_probs_inds.shape[0]
+
             if find_correct:
-                # i.e solves the combinatorial problem, that is gives "000111"
-                ranks = datum[invm]["ranks"]
-                eff = np.sum(
-                    (ranks - (ranks % (2 if assume_symmetric else 1))) == 0
-                ) / len(ranks)
+                bs_ind = int(cor_bs, 2)
+                bs_swap_ind = int(swap(cor_bs), 2)
             else:
-                # i.e. found the minimum energy bitstring
-                # For some reason, the `min_bistrings` are saved as ints? I
-                # don't know why and it's too late to change it, so we need to
-                # turn them back into bitstrings by padding the ints with 0s
-                min_bitstrings = [
-                    f"{int(bs):0>6}" for bs in datum[invm]["min_bitstrings"]
-                ]
-                alg_bitstrings = get_bitstrings(datum=datum)[INVMS.index(invm)]
-                eff = np.sum(min_bitstrings == alg_bitstrings) / len(min_bitstrings)
+                bs_ind = np.array([int(bs, 2) for bs in min_bitstrings])
+                bs_swap_ind = np.array([int(swap(bs), 2) for bs in min_bitstrings])
 
-            datum_effs.append(eff)
-        effs.append(datum_effs)
+            # Make a mask of where our wanted bitstrings are. We are seeing if
+            # the bitstrings with the max probability as stored in
+            # `max_probs_inds` ARE these wanted bitstrings. Then we sum up all
+            # instances where that is true.
+            is_match = (max_probs_inds == bs_ind) | (max_probs_inds == bs_swap_ind)
+            effs[dind, iind] = is_match.sum() / tot_evts
 
-    return np.array(effs)
+    return effs
+
+
+def find_probabilities(
+    data: NDArray[DatumType],
+    find_correct: bool = True,
+    etype: str | Sequence[str] = "ttbar",
+) -> NDArray[NDArray[float]]:
+    """
+    Similar to `find_efficiency`, this will instead calculate, not how many
+    events chose a specific bitstring as the most probable, but what the actual
+    probabilities of these bitstrings are. This can be considered as a form of
+    "confidence". For example, an algorithm could have an efficiency of 90%
+    (that is, for 90% of events, the algorithm chose the correct bitstring
+    with the most probability) but maybe it only chooses said bitstring with 25%
+    probability. In this case, the quantum state is in a large superposition of
+    states.
+
+    Parameters:
+    data - An array of data for runs as created by the `JobRunner` class and
+        saved as an .npz file.
+    find_correct (default True) - If True, the "correct" bitstring is the one
+        that solves the problem, in our case it is "000111". If False, the
+        "correct" bitstring is the one that minimizing the Hamiltonian.
+    etype (default "ttbar") - The event type. If a string, it will apply it the
+        same data type to every element of `data`. If a list, it will be
+        element-specific.
+    """
+    # Number of data to go over
+    data_len = len(data)
+    n_bins = len(INVMS) - 1
+    # Turn into a list of all the same etype
+    if isinstance(etype, str):
+        etype = [etype] * data_len
+
+    probs = np.empty((data_len, n_bins), dtype=np.float64)
+    for dind, (datum, etype) in enumerate(zip(data, etype)):
+        # The correct bitstring
+        cor_bs = SYM_TRUE_BS_DICT[etype]
+
+        for iind, invm in enumerate(INVMS[:-1]):
+            invm_datum = datum[invm]
+            # Bitstrings minimizing the energy for each event
+            min_bitstrings = invm_datum["min_bitstrings"]
+            # All probabilities for each event
+            evt_probs = invm_datum["probs"]
+
+            if find_correct:
+                # Probabilities for CORRECT bitstring
+                bs_ind = int(cor_bs, 2)
+                bs_swap_ind = int(swap(cor_bs), 2)
+
+                bs_probs = evt_probs[:, bs_ind] + evt_probs[:, bs_swap_ind]
+            else:
+                # Probabilities for MINIMUM bitstrings
+                bs_ind = np.array([int(bs, 2) for bs in min_bitstrings])
+                bs_swap_ind = np.array([int(swap(bs), 2) for bs in min_bitstrings])
+
+                bs_probs = np.take_along_axis(evt_probs, bs_ind[:, None]).squeeze()
+                bs_probs += np.take_along_axis(evt_probs, bs_swap_ind[:, None]).squeeze()
+
+            probs[dind, iind] = bs_probs.mean()
+    return probs
 
 
 def parse_with_metadata(
-    infos: Sequence[Sequence[str]], metadata: NDArray[str], one_per: bool = True
-):
+    infos: Sequence[Sequence[str]] | Sequence[str],
+    metadata: NDArray[str],
+    one_per: bool = True,
+) -> NDArray[NDArray[str]] | NDArray[str]:
     """
     Given a list of keywords, it will find the data that corresponds to that
     data. For example, if `infos=[["qaoa", "max"], ["maqaoa" "5"]], it will find
@@ -128,35 +151,45 @@ def parse_with_metadata(
 
     Parameters:
     infos - A sequence where each element contains info to search through the
-        metadata for. Each element is a different search query.
+        metadata for. Each element is a different search query. OR just a single
+        search query, i.e. one element from the first sentence.
     metadata - An array of elements of the list output by the `parse_data`
         method. Contains the metadata info for a run.
     """
-    # Ignore number of events
-    metadata = metadata[:, :-1]
+    is_single = isinstance(infos[0], str)
+    if is_single:
+        infos = [infos]
+
     indices = []
     # Iterate over each search query
     for info in infos:
         mask = np.ones(len(metadata), dtype=bool)
         # Iterate over each search term
         for ele in info:
+            # Convert float to string
+            if isinstance(ele, float):
+                ele = f"{ele:.3f}"
             # If that term isn't in a metadatum, then it gets a False mask
             mask &= (metadata == ele).any(axis=1)
 
         # Just warn if the mask is completely False
-        if not mask.any():
-            print(f"Warning: didn't find any results for: {info}")
         if one_per and mask.sum() > 1:
             raise Exception(
                 f"`one_per=True` but we have {mask.sum()} results for: {info}."
+                f" Found indices: {np.where(mask)[0]}"
             )
+        if not mask.any():
+            print(f"Warning: didn't find any results for: {info}")
 
         indices.append(np.where(mask)[0])
+
+    if is_single:
+        return indices[0]
     return np.concatenate(indices)
 
 
 def int_to_bin(
-    arr: NDArray[Union[str, int]], N: int, dtype: str = "bits"
+    arr: NDArray[str | int], N: int, dtype: Literal["bits", "str"] = "bits"
 ) -> NDArray[str]:
     """
     Converts an array of ints (or strings of ints) into their binary values.
@@ -172,8 +205,8 @@ def int_to_bin(
 
 
 def get_2dhist_invms(
-    datum: datum_type, metadatum: NDArray[str], by_invm_bin: bool = False
-) -> NDArray[Union[tuple[float, float], NDArray[tuple[float, float]]]]:
+    datum: DatumType, metadatum: NDArray[str], by_invm_bin: bool = False
+) -> NDArray[tuple[float, float] | NDArray[tuple[float, float]]]:
     """
     Gets the masses of the final state particles based on what the algorithm
     chose at the most likely bitstring, i.e. if "110000" was chosen, then it
@@ -205,7 +238,7 @@ def get_2dhist_invms(
     return invms.reshape(-1, 2)
 
 
-def get_bitstrings(datum: datum_type) -> NDArray[str]:
+def get_chosen_bitstrings(datum: DatumType) -> NDArray[str]:
     """
     For the given run, returns the bitstring chosen with the highest probability
     by the algorithm for each event.
